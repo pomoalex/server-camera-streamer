3286791f1528a14407506b7aa09728facde389dc
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2020-06-03 17:15:49 +0300

    Updated flask app port to default http port 80

--- server.py
+++ server.py
@@ -122,5 +122,5 @@ def video_feed():
 
 if __name__ == '__main__':
     launch_streaming_threads()
-    app.run(host='0.0.0.0', port=8000, debug=True,
+    app.run(host='0.0.0.0', port=80, debug=True,
             threaded=True, use_reloader=False)
