760bfc603e945090169b75ced5bffb860918795d
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2020-06-03 16:36:57 +0300

    Transformed app into flask web app

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ from threading import Thread, Lock
 import cv2
 import imagezmq
 import imutils
+import numpy as np
+from flask import Flask
+from flask import Response
+from flask import render_template
 from imutils import build_montages
 
 from utils import get_network_device_ip
@@ -17,20 +21,6 @@ frame_dict = {}
 live_clients = {}
 LIVENESS_CHECK_SECONDS = 5
 MAX_INACTIVITY = 3
-
-
-def display_montage():
-    global frame_dict
-    frames = list(frame_dict.values())
-    count = len(frames)
-    if count > 0:
-        (h, w) = frames[0].shape[:2]
-        columns = math.ceil(math.sqrt(count))
-        rows = columns if count > columns * (columns - 1) else columns - 1
-        montage = build_montages(frames, (w, h), (columns, rows))[0]
-        cv2.imshow("Remote camera streaming", montage)
-    else:
-        cv2.destroyWindow("Remote camera streaming")
 
 
 def add_frame_to_frame_dict(frame, host_name):
@@ -42,10 +32,10 @@ def add_frame_to_frame_dict(frame, host_name):
     frame_dict[host_name] = frame
 
 
-def check_liveness(lock):
+def check_liveness():
     while True:
         time.sleep(LIVENESS_CHECK_SECONDS)
-        with lock:
+        with stream_lock:
             for (host_name, start_time) in list(live_clients.items()):
                 if (datetime.now() - start_time).seconds > MAX_INACTIVITY:
                     print("[INFO] lost connection to {}".format(host_name))
@@ -53,7 +43,7 @@ def check_liveness(lock):
                     frame_dict.pop(host_name)
 
 
-def receive_streams(lock):
+def receive_streams():
     image_hub = imagezmq.ImageHub()
 
     print("[INFO] Started listening at " + get_network_device_ip())
@@ -62,7 +52,7 @@ def receive_streams(lock):
         (host_name, frame) = image_hub.recv_image()
         image_hub.send_reply(b'OK')
 
-        with lock:
+        with stream_lock:
             if host_name not in live_clients.keys():
                 print("[INFO] receiving data from {}...".format(host_name))
 
@@ -70,30 +60,67 @@ def receive_streams(lock):
             add_frame_to_frame_dict(frame, host_name)
 
 
-def serve_streams():
-    lock = Lock()
-
-    stream_receiver_thread = Thread(target=receive_streams, args=(lock,))
+def launch_streaming_threads():
+    stream_receiver_thread = Thread(target=receive_streams)
     # daemon threads are terminated after main thread dies
     stream_receiver_thread.daemon = True
     stream_receiver_thread.start()
 
-    liveness_check_thread = Thread(target=check_liveness, args=(lock,))
+    liveness_check_thread = Thread(target=check_liveness)
     # daemon threads are terminated after main thread dies
     liveness_check_thread.daemon = True
     liveness_check_thread.start()
 
+
+def build_montage():
+    frames = list(frame_dict.values())
+    count = len(frames)
+    if count > 0:
+        (h, w) = frames[0].shape[:2]
+        columns = math.ceil(math.sqrt(count))
+        rows = columns if count > columns * (columns - 1) else columns - 1
+        montage = build_montages(frames, (w, h), (columns, rows))[0]
+        return montage.copy()
+    else:
+        frame = np.zeros(shape=[375, 500, 3], dtype=np.uint8)
+        text = 'No camera available'
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+        cv2.putText(frame, 'No camera available', ((500 - text_size[0]) // 2, 375 // 2),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        return frame
+
+
+def serve_streams():
     while True:
-        with lock:
-            display_montage()
+        with stream_lock:
+            output_frame = build_montage()
 
-        # if the `q` key was pressed, break from the loop
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
+            (flag, encodedImage) = cv2.imencode(".jpg", output_frame)
 
-    cv2.destroyWindow("Remote camera streaming")
+            if not flag:
+                continue
+
+        # yield the output frame in the byte format
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(encodedImage) + b'\r\n')
+
+
+app = Flask(__name__)
+stream_lock = Lock()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(serve_streams(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == '__main__':
-    serve_streams()
+    launch_streaming_threads()
+    app.run(host='localhost', port=8000, debug=True,
+            threaded=True, use_reloader=False)
